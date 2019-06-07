590c5827141a4ab096218172805ef40e9c69b4ab
Antonio Blandin <antonio.blandin25@gmail.com>
2019-06-07 01:55:24 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance > @amount && @status
+    if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
