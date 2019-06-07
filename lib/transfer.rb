d65eea3a6002f6cf21098e22f028389d880f2552
Antonio Blandin <antonio.blandin25@gmail.com>
2019-06-07 01:51:12 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -9,26 +9,15 @@ class Transfer
   end
 
   def both_valid?
-    if sender.valid? && receiver.valid?
-      true
-    else
-      false
-    end
+    
   end
 
   def execute_transaction
-    if @sender.balance < @amount
-      @status = "rejected"
-      return "Transaction rejected."
-  end
+    
 end
  
   def reverse_transfer
-    if @status == "complete"
-      @sender.deposit(@amount) 
-      @receiver.deposit(@amount * -2)
-      @status = "reversed"
-    end
+    
   end
 
 end
